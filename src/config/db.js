013d955415b8f74a6110c8fa71be243eb06f7193
Rohit Yadav <101393562+rohity0@users.noreply.github.com>
2024-06-04 00:37:26 +0530

    Add files via upload

---
+++ src/config/db.js
@@ -0,0 +1,20 @@
+
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      'mongodb+srv://yadavry09:9OKd6j0r6Jtp7rQM@cluster0.r40tyug.mongodb.net/Scrap',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+    );
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection failed', error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
